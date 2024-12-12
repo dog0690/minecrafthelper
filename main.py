2643d1f101ca6aff954910dc3c853bc0404e29f5
dog0690 <charleymaivn@gmail.com>
2024-12-12 01:57:55 -0500

    choice functions

--- main.py
+++ main.py
@@ -1,149 +1,19 @@
-import sys
-import os
-#test = open(r"test.txt","r")
-folder_path = (r"C:\Users\charl\OneDrive\Desktop\coding\minecrafthelper\worlds")
+from test_functions import *
+folder_path = (r'C:\Users\charl\OneDrive\Desktop\coding\minecrafthelper\worlds')
 contents = os.listdir(folder_path)
-def divider():
-    print("==========================")
-def sub_divider():
-    print("--------------------------")
-print(f"Hello, this is your minecraft helper\nHow can I help you?")
-divider()
-option = ["A) View Saves", "B) Exit"]
-#saved_world = []
-#function
-#lines = test.readlines()
-#line_count = len(lines)
-def close():
-    sys.exit
+prompt1 = ('[A] Make World', '[B] Enter Worlds', '[C] Delete World')
 def main():
-    for i in option:
-        print (i)     
-    sub_divider()
-main()
-#def world_list():
-#    for i in range(line_count):
-#        (i+1, test.readline())
-#Option A Menu
-def new_save(name):
     clear_terminal()
-    sub_divider()
-#    test = open("test.txt", "a")
-    os.chdir(r"C:\Users\charl\OneDrive\Desktop\coding\minecrafthelper\worlds")
-    os.mkdir(name)
-    os.chdir(r"C:\Users\charl\OneDrive\Desktop\coding\minecrafthelper\worlds" + r"\\" +name )
-    with open(name, "w+") as file:
-        folder(file, name)
-    sub_divider()
-
-def world_logs(name):
-    #os.chdir(r"C:\Users\charl\OneDrive\Desktop\coding\minecrafthelper")
-    world_logs = (r"C:\Users\charl\OneDrive\Desktop\coding\minecrafthelper\world_logs.txt")
-    with open(world_logs, "a") as worlds:
-        worlds.write(name + "\n")
-    return
-#folder management system
-def folder(file, name):
-    file.write(name)
-    lines = file.readlines()
-    line_count = len(lines)
-    for i in range(line_count):
-        print(i+1, file.readline().strip())
-def delete_save():
-    world_pos = int(input("Which world do you want to delete?\n"))
-#    world_name = lines[world_pos -1]
-#    print(f"are you sure you want to delete {world_pos}, {world_name}?")
-#    def delete_world():
-#        world_type = input("Please type to confirm:")
-#        with open("test.txt", "w") as f:
-#            for line in lines:
-#                if line.strip("\n") != world_type:
-#                    f.write(line)
-#        print("Deleted!")
-#        A()
-    def choice(option):
-        if option == "y":
-#            delete_world()
-            pass
-        elif option == "n":
-            print("No")
-        else:
-            print("retry")
-            choice()
-    choice(input("(Y) Yes or N (No)\n"))
-#Main Menu
-def A():
-#    test = open(r"test.txt","r")
-    option = ["A) New save", "B) Delete Save","C) Enter world", "D) Return"]
-    print("YOUR WORLDS")
-    sub_divider()
     for item in contents:
         print(item)
-    sub_divider()
-    for i in option:
-        print(i)
-    def choice(option):
-        if option =="a":
-            name = input("What's the name?\n")
-            new_save(name)
-            world_logs(name)
-        elif option =="b":
-            delete_save()
-        elif option == "c":
-            C()
-        elif option =="d":
-            close()
-            
-    divider()
-    choice(input("what do you want to do?\n"))
-    divider()
-def C():
-    clear_terminal()
-    divider()
-    for item in contents:
-        print(item)
-    sub_divider()
-    name = input("What world do you want to enter?\n")
-    world = (r"C:\Users\charl\OneDrive\Desktop\coding\minecrafthelper\worlds"+"\\"+name)
-    os.chdir(world)
-    with open(name, "a+") as file:
-        content = file.read()
-        print(content)
-        choices = ["A) Add information", "B) Delete Information", "C) Return"]
-        for i in choices:  
-            print(i)
-        c_choice(input("what do you want to do? \n"), name)
+    print("what can I help you with today?")
+    multiples(prompt1)
+    answer1 = input("\n")
+    choice(answer1, make_world(), enter_world(), delete_world())
+def make_world():
     pass
-def c_choice(option, name):
-    if option == "a":
-        with open(name, "a") as file:
-            information = input("what do you want to add?\n")
-            file.write(information + "\n")
-            file.close()
-    else:
-        pass
-#Menu
-def choice(option):
-    
-    divider()
-    for item in contents:
-        print(item)
-    sub_divider()
-    if option == "a":
-        clear_terminal()
-        A()
-    elif option == "b":
-        clear_terminal()
-        close()
-    elif option == "c":
-        C()
-    else:
-        print("retry")
-        clear_terminal()
-        choice()
-def clear_terminal():
-    if os.name == "nt":
-        os.system('cls')
-    else:
-        os.system("clear")
-choice(input(""))
+def enter_world():
+    pass
+def delete_world():
+    pass
+main()
---
+++ test_functions.py
@@ -0,0 +1,35 @@
+import os
+import mysql.connector
+import time
+import shutil
+folder_path = (r'C:\Users\charl\OneDrive\Desktop\coding\minecrafthelper\worlds')
+def choice(choice, f1, f2, f3):
+    if choice == 'a':
+        f1
+    elif choice == 'b':
+        f2
+    elif choice == 'c':
+        f3
+
+def multiples(option_c):
+    for i in range(len(option_c)):
+        print(f"{option_c[i]}")
+
+def clear_terminal():
+    os.system('cls')
+
+def test_makew(name, s):
+    worlds = (r"C:\Users\charl\OneDrive\Desktop\coding\minecrafthelper\worlds")
+    wor = (r"C:\Users\charl\OneDrive\Desktop\coding\minecrafthelper\worlds" + r"\\" +name )
+    os.chdir(worlds)
+    os.mkdir(name)
+    os.chdir(wor)
+    with open(name, "w+") as file:
+        file.write("first save")
+    #Testing Portion
+    time.sleep(s)
+    os.chdir(worlds)
+    shutil.rmtree(wor)
+    print('test completed')
+world_name = 'test1'
+test_makew(world_name, 5)
